a8fee1eb72bff195573f4e464791ad6835762635
poujcarl <joaocarlos.analista.ti@gmail.com>
2014-06-21 15:23:35 -0300

    commit2

--- LogIn/src/LogIn.java
+++ LogIn/src/LogIn.java
@@ -5,7 +5,15 @@ public class LogIn {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		 String user = JOptionPane.showInputDialog("Digite o usuário: ");
+         String pass = JOptionPane.showInputDialog("Digite a senha: ");
+       
+         if ((user.equals("seu nome")) && (pass.equals("123")) {
+                                                                                             JOptionPane.showMessageDialog(null,"Oi");
+        }
+        else{
+               JOptionPane.showMessageDialog(null, "Usuário ou senha inválido!!!");
+        }
 
 	}
 
